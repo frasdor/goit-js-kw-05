3370e9ecb53e007d27df82385caf02ce8b617888
frasdor <frasunkiewicz.dorota@gmail.com>
2024-08-22 14:45:45 +0200

    added task 1 and task2

--- js/task-2.js
+++ js/task-2.js
@@ -1,45 +1,63 @@
-function calcAverageCalories(days) {
-    if (days.length === 0) {
-        return 0; 
+const allUsers = [
+    {
+      name: "Moore Hensley",
+      friends: ["Sharron Pace"]
+    },
+    {
+      name: "Sharlene Bush",
+      friends: ["Briana Decker", "Sharron Pace"]
+    },
+    {
+      name: "Ross Vazquez",
+      friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"]
+    },
+    {
+      name: "Elma Head",
+      friends: ["Goldie Gentry", "Aisha Tran"]
+    },
+    {
+      name: "Carey Barr",
+      friends: ["Jordan Sampson", "Eddie Strong"]
+    },
+    {
+      name: "Blackburn Dotson",
+      friends: ["Jacklyn Lucas", "Linda Chapman"]
+    },
+    {
+      name: "Sheree Anthony",
+      friends: ["Goldie Gentry", "Briana Decker"]
     }
-
-    let totalCalories = 0;
-    for (let i = 0; i < days.length; i++) {
-        totalCalories += days[i].calories;
-    }
-
-    const averageCalories = totalCalories / days.length;
-    return averageCalories;
-}
-
-console.log(
-    calcAverageCalories([
-      { day: "monday", calories: 3010 },
-      { day: "tuesday", calories: 3200 },
-      { day: "wednesday", calories: 3120 },
-      { day: "thursday", calories: 2900 },
-      { day: "friday", calories: 3450 },
-      { day: "saturday", calories: 3280 },
-      { day: "sunday", calories: 3300 }
-    ])
-); // 3180
+  ];
   
-console.log(
-    calcAverageCalories([
-      { day: "monday", calories: 2040 },
-      { day: "tuesday", calories: 2270 },
-      { day: "wednesday", calories: 2420 },
-      { day: "thursday", calories: 1900 },
-      { day: "friday", calories: 2370 },
-      { day: "saturday", calories: 2280 },
-      { day: "sunday", calories: 2610 }
-    ])
-); // 2270
-  
-console.log(
-    calcAverageCalories([])
-); // 0
-  
-// Zadanie 2. Obliczanie kalorii
+  const getUsersWithFriend(allUsers, friendName) => {
+    
+    return allUsers.filter(user=>user.friends.includes(friendName));
+    
+  };
 
-// Napisz funkcję calcAverageCalories(days), która zwraca średnią dzienną wartość liczby kalorii, które sportowiec spożywał w ciągu tygodnia. Funkcja oczekuje jednego parametru: days — tablicy obiektów. Każdy obiekt opisuje dzień tygodnia oraz liczbę kalorii calories, spożytych przez sportowca tego dnia. Weź poniższy kod i wstaw go po deklaracji swojej funkcji, aby sprawdzić poprawność jej działania. W konsoli zostaną wyświetlone wyniki jej wywołań.
+  console.log(getUsersWithFriend(allUsers, "Briana Decker")); 
+  // [
+  //   {
+  //     name: "Sharlene Bush",
+  //     friends: ["Briana Decker", "Sharron Pace"]
+  //   },
+  //   {
+  //     name: "Sheree Anthony",
+  //     friends: ["Goldie Gentry", "Briana Decker"]
+  //   }
+  // ]
+  
+  console.log(getUsersWithFriend(allUsers, "Goldie Gentry"));
+  // [
+  //   {
+  //     name: "Elma Head",
+  //     friends: ["Goldie Gentry", "Aisha Tran"]
+  //   },
+  //   {
+  //     name: "Sheree Anthony",
+  //     friends: ["Goldie Gentry", "Briana Decker"]
+  //   }
+  // ]
+  
+  console.log(getUsersWithFriend(allUsers, "Adrian Cross" )); // []
+  
